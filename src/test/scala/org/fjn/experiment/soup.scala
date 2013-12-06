234f2823faf0d1636d75f0d5d0916a07e8d00cfc
fran <frannuca@gmail.com>
2013-12-07 00:04:57 +0100

    adding multithreading to soup

--- src/test/scala/org/fjn/experiment/soup.scala
+++ src/test/scala/org/fjn/experiment/soup.scala
@@ -2,58 +2,96 @@ package org.fjn.experiment
 
 import scala.collection.immutable.IndexedSeq
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.{Await, ExecutionContext, Future}
 
-class Soup(matrix:Array[Array[Char]]) {
-  val M =  matrix.size
+class Soup(matrix: Array[Array[Char]]) {
+  val M = matrix.size
   val N = matrix.head.size
 
+  import ExecutionContext.Implicits.global
 
-  def containsWord(word:String)={
 
-    (for{i<- 0 until M
-        j<- 0 until N}
-    yield{
-      val history = new ListBuffer[(Int,Int)]()
+  def containsWordSingleThreaded(word: String) = {
 
-      resolve(word.toCharArray,(i,j),history)
-      history
 
-    }).filter(h => h.length==word.length)
+      (for {i <- 0 until M
+            j <- 0 until N}
+      yield {
+
+
+          val history = new ListBuffer[(Int, Int)]()
+
+          resolve(word.toCharArray, (i, j), history)
+          history
+
+
+
+      }).filter(lst => lst.length == word.length)
+
 
   }
 
-  def resolve(word:Array[Char],position:(Int,Int),history:ListBuffer[(Int,Int)]):Boolean ={
+  private def solvePartially(n0:Int,n1:Int,word:String): Future[IndexedSeq[ListBuffer[(Int, Int)]]] ={
+
+    scala.concurrent.future {
+      (for {i <- n0 until n1
+            j <- 0 until M}
+      yield {
 
 
-    val (i,j) = position
+          val history = new ListBuffer[(Int, Int)]()
+
+          resolve(word.toCharArray, (i, j), history)
+          history
 
 
-    if(history.contains(position) || i<0 || j<0 || j>=N || i>=M) {
+
+      })
+    }
+
+  }
+    def containsWord(word: String): IndexedSeq[ListBuffer[(Int, Int)]] = {
+
+      var n0 =0
+      var n1 = 0
+      val split = 4
+
+      (for { i<- 1 to split} yield{
+        solvePartially((N/split*(i-1)).toInt,(N/split*i).toInt,word)
+      }).flatMap(f => Await.result(f, scala.concurrent.duration.Duration.Inf)).filter(lst => lst.length == word.length)
+    }
+
+  def resolve(word: Array[Char], position: (Int, Int), history: ListBuffer[(Int, Int)]): Boolean = {
+
+
+    val (i, j) = position
+
+
+    if (history.contains(position) || i < 0 || j < 0 || j >= N || i >= M) {
 
       false
     }
 
-    else if(word.length==1 && matrix(i)(j)==word.head)  {
-      history +=  position
+    else if (word.length == 1 && matrix(i)(j) == word.head) {
+      history += position
       true
     }
-    else{
+    else {
       val letter = matrix(i)(j)
-      if(letter != word.head)
+      if (letter != word.head)
         false
-      else{
+      else {
 
-         history +=  position
+        history += position
 
-         resolve(word.tail,(i+1,j),history)||
-         resolve(word.tail,(i-1,j),history)||
-         resolve(word.tail,(i,j+1),history)||
-         resolve(word.tail,(i,j-1),history)||
-         resolve(word.tail,(i+1,j+1),history)||
-         resolve(word.tail,(i-1,j+1),history) ||
-         resolve(word.tail,(i-1,j-1),history) ||
-         resolve(word.tail,(i+1,j-1),history)
-
+        resolve(word.tail, (i + 1, j), history) ||
+          resolve(word.tail, (i - 1, j), history) ||
+          resolve(word.tail, (i, j + 1), history) ||
+          resolve(word.tail, (i, j - 1), history) ||
+          resolve(word.tail, (i + 1, j + 1), history) ||
+          resolve(word.tail, (i - 1, j + 1), history) ||
+          resolve(word.tail, (i - 1, j - 1), history) ||
+          resolve(word.tail, (i + 1, j - 1), history)
 
 
       }
@@ -65,39 +103,66 @@ class Soup(matrix:Array[Array[Char]]) {
 }
 
 
-object test extends  App{
+object test extends App {
 
+  def time[T](str: String)(thunk: => T): T = {
+    print(str + "... ")
+    val t1 = System.currentTimeMillis
+    val x = thunk
+    val t2 = System.currentTimeMillis
+    println((t2 - t1) + " msecs")
+    x
+  }
   val random = new scala.util.Random
   val mm: Array[Array[Char]] =
-    (for{
-      i <- 0 until 100
-  }yield {
-      (for(j <- 0 until 100)yield{
+    (for {
+      i <- 0 until 1000
+    } yield {
+      (for (j <- 0 until 10000) yield {
         (random.nextInt(256)).toChar
       }).toArray
     }).toArray
 
-  mm(3)(3)='s'
-  mm(4)(4)='a'
-  mm(5)(5)='l'
-  mm(6)(6)='s'
-  mm(7)(7)='a'
-  mm(7)(8)='p'
-  mm(7)(9)='a'
-  mm(7)(10)='r'
-  mm(7)(11)='a'
-  mm(7)(12)='t'
-  mm(7)(13)='o'
-  mm(7)(14)='d'
-  mm(7)(15)='o'
-  mm(7)(16)='s'
 
+  mm(113)(113) = 's'
+  mm(114)(114) = 'a'
+  mm(115)(115) = 'l'
+  mm(116)(116) = 's'
+  mm(117)(117) = 'a'
+  mm(118)(118) = 'p'
+  mm(119)(119) = 'a'
+  mm(120)(120) = 'r'
+  mm(121)(121) = 'a'
+  mm(122)(122) = 't'
+  mm(123)(123) = 'o'
+  mm(124)(124) = 'd'
+  mm(125)(125) = 'o'
+  mm(126)(126) = 's'
 
-
-
+  mm(13)(13) = 's'
+  mm(14)(14) = 'a'
+  mm(15)(15) = 'l'
+  mm(16)(16) = 's'
+  mm(17)(17) = 'a'
+  mm(17)(18) = 'p'
+  mm(17)(19) = 'a'
+  mm(17)(20) = 'r'
+  mm(17)(21) = 'a'
+  mm(17)(22) = 't'
+  mm(17)(23) = 'o'
+  mm(17)(24) = 'd'
+  mm(17)(25) = 'o'
+  mm(17)(26) = 's'
 
   val s = new Soup(mm)
-  val aa = s.containsWord("salsaparatodos")
-  println(aa)
+  time("time single threaded"){
+  val aa = s.containsWordSingleThreaded("salsaparatodos")
+  println(aa.mkString("\n"))
+  }
+
+  time("time multi threaded"){
+    val aa = s.containsWord("salsaparatodos")
+    println(aa.mkString("\n"))
+  }
 
 }
